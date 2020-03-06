431a8620636e88ac57ee88ec54a8c346af03dd6d
Juan Luis <Juan Luis@192.168.1.248>
2020-03-06 12:51:29 +0100

    no se hizo cambios. esto es un abrueba

---
+++ src/minicalculadoraejemplo/MiniCalculadoraejemplo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minicalculadoraejemplo;
+
+/**
+ *
+ * @author Juan Luis
+ */
+public class MiniCalculadoraejemplo {
+
+    private double num;
+
+    public MiniCalculadoraejemplo(double n) {
+        this.num = n;
+    }
+
+    /**
+     *
+     * @param n número que le pasamos para calcular valor absoluto
+     * @return devuelve valor del valor absoluto
+     */
+    public double valorAbsoluto(double n) {
+        return Math.abs(n);
+    }
+
+    /**
+     *
+     * @param n número que le pasamos para calcular la raiz cuadrada
+     * @return devuelve valor de la raiz cuadrada
+     */
+    public double raizCuadrada(double n) {
+        return Math.sqrt(n);
+    }
+
+}
